9f4ec85f10a6f135bf98e548a4dceb5426b6a4c0
brokemyownankles <leo06schuster@gmail.com>
2020-08-10 14:34:13 +0200

    added basic (but unfinished) monitor

--- monitor.py
+++ monitor.py
@@ -1,1 +1,21 @@
-print("Hello, World!")
+import requests
+import json
+from bs4 import BeautifulSoup
+import randomheaders
+
+# URL and stuff
+URL = 'https://www.nike.com/de/launch?s=upcoming'
+ua = randomheaders.LoadHeader()
+headers = { 'user-agent': 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.7.13) Gecko/20060501 Epiphany/2.14', 'referer': 'https://www.nike.com/de/launch' }
+
+# Monitor
+def monitor():
+    r = requests.get(URL, headers=headers).text
+    res = BeautifulSoup(r, 'lxml')
+
+    for hrefs in BeautifulSoup.find_all('a', class_='pb2-sm va-sm-t ncss-col-sm-12 ncss-col-md-6 ncss-col-lg-4 pb4-md '
+                                                    'prl0-sm prl2-md ncss-col-sm-6 ncss-col-lg-3 pb4-md prl2-md '
+                                                    'pl0-md pr1-md d-sm-h d-md-ib'):
+        print(hrefs.get('href'))
+    print(res)
+monitor()
