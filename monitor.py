115a918f84cae7a1d64394bffeeb3e99c503963a
brokemyownankles <leo06schuster@gmail.com>
2020-08-11 21:10:19 +0200

    added basic (but unfinished) monitor

--- monitor.py
+++ monitor.py
@@ -13,9 +13,9 @@ def monitor():
     r = requests.get(URL, headers=headers).text
     res = BeautifulSoup(r, 'lxml')
 
-    for hrefs in BeautifulSoup.find_all('a', class_='pb2-sm va-sm-t ncss-col-sm-12 ncss-col-md-6 ncss-col-lg-4 pb4-md '
+    for hrefs in res.find_all('figure', class_='pb2-sm va-sm-t ncss-col-sm-12 ncss-col-md-6 ncss-col-lg-4 pb4-md '
                                                     'prl0-sm prl2-md ncss-col-sm-6 ncss-col-lg-3 pb4-md prl2-md '
                                                     'pl0-md pr1-md d-sm-h d-md-ib'):
         print(hrefs.get('href'))
-    print(res)
+    #print(res)
 monitor()
